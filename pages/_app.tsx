e543b971b7f79832ec5df1d63a98816bd4f0e654
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-04-27 07:48:05 +0600

    added head and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,23 @@ import { Provider } from "react-redux";
 import store from "@/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
+import Head from "next/head";
 
 let persistor = persistStore(store);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Component {...pageProps} />
-      </PersistGate>
-    </Provider>
+    <>
+      <Head>
+        <title>Maker Market</title>
+        <meta name="description" content="Welcome to Maker Market" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </>
   );
 }
