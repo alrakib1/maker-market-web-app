c596339a733adfa84790dcf9dcd925caa7dc8e3c
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-05-11 06:34:24 +0600

    Add footer component and links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.scss";
 import Header from "@/components/header";
-
-const inter = Inter({ subsets: ["latin"] });
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <main>
-    <Header/> 
-     
+      <Header />
+      <Footer />
     </main>
   );
 }
